8980f866eb138f933e2aa2280ca5a7ec639b548f
NicolasPreiti <nicolaspreiti45@gmail.com>
2022-09-01 03:40:44 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000
-  },
-  preview: {
-    port: process.env.PORT || 3000
-  }
+  plugins: [react()]
 });
